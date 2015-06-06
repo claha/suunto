c8a07e49580023b7df45359239a08de95448857e
Claes Hallstrom <hallstrom.claes@gmail.com>
2015-06-06 21:58:57 +0200

    Add/Mul support for suunto.duration.Time

--- suunto/duration.py
+++ suunto/duration.py
@@ -14,3 +14,19 @@ class Time(object):
 
     def __repr__(self):
         return self.__str__()
+
+    def __add__(self, other):
+        if isinstance(other,Time):
+            return Time(s=self.s+other.s)
+        raise SuuntoException('ERROR: Cannot perform addition with {0} and {1}'.format(type(self), type(other)))
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        if isinstance(other,int):
+            return Time(s=other*self.s)
+        raise SuuntoException('ERROR: Cannot perform multiplication with {0} and {1}'.format(type(self), type(other)))
+
+    def __rmul__(self,other):
+        return self.__mul__(other)

b9c87451e2275b884dab017dcb834ff39972cf39
Claes Hallstrom <hallstrom.claes@gmail.com>
2015-06-07 13:53:44 +0200

    Better code generation, comments in generated code

--- suunto/workout.py
+++ suunto/workout.py
@@ -31,27 +31,23 @@ class Workout(object):
 
         # Generate
         wr('if (SUUNTO_DURATION == 0) {')
-        wr('  prefix = "%s";' % (self.steps[0][0]))
-        wr('  RESULT = %d;' % (self.steps[0][1].s))
         wr('  STEP = 0;')
-        wr('  ELAPSEDTIME = 0;')
+        wr('  PREVSTEPSDURATION = 0;')
         wr('}')
         wr('')
-
+        
         for i,step in enumerate(self.steps):
-            if i == 0:
-                wr('if (STEP == %d && SUUNTO_DURATION > 0) {' % (i))
-            else:
-                wr('if (STEP == %d) {' % (i))
+            wr('/* %s - %ss */' % (step[0], step[1].s))
+            wr('if (STEP == %d) {' % (i))
             wr('  prefix = "%s";' % (step[0]))
-            wr('  RESULT = %d - (SUUNTO_DURATION - ELAPSEDTIME);' % (step[1].s))
+            wr('  RESULT = %d - (SUUNTO_DURATION - PREVSTEPSDURATION);' % (step[1].s))
 
             if i == len(self.steps) - 1:
                 wr('  if (RESULT <= 0) {')
             else:
                 wr('  if (RESULT == 0) {')
-                wr('    STEP = STEP + 1;')
-                wr('    ELAPSEDTIME = SUUNTO_DURATION;')
+                wr('    STEP = %d; /* %s */' % (i+1,self.steps[i+1][0]))
+                wr('    PREVSTEPSDURATION = SUUNTO_DURATION;')
             wr('    Suunto.alarmBeep();')
             wr('  }')
             wr('}')
